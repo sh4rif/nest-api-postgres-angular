8e6ff93eeda7bcb613b9677e3161f2d45b2c012a
Usman <usman.sharif@gigalabs.co>
2021-06-28 17:56:53 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+      host: process.env.POSTGRES_HOST,
+      port: (process.env.PORT && parseInt(process.env.POSTGRES_PORT)) || 5432,
+      database: process.env.POSTGRES_DATABASE,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
